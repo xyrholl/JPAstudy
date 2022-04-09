18dd25a6adfa1f477a85bd7234e7f7ffd9de674b
LeeSangYoeb <xyrho.v@gmail.com>
2022-04-09 20:31:11 +0900

    Chore 주문검색 개발
    - JPA 동적쿼리 표준인 Criteria 사용
    직관적으로 쿼리및 로직을 알아 보기 힘듬. QueryDsl 사용 권장.

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -79,33 +79,33 @@ public class Order {
 
     }
 
-        //==비즈니스 로직==//
-        /**
-         * 주문 취소
-         */
-        public void cancel(){
-            if(delivery.getStatus() == DeliveryStatus.COMP){
-                throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
-            }
-            this.setStatus(OrderStatus.CANCEL);
-            for (OrderItem orderItem : orderItems){
-                orderItem.cancel();
-            }
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
         }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+    }
 
-            //==조회 로직==//
-            /**
-             * 전체 주문 가격 조회
-             */
-            public int getTotalPrice(){
-                /*
-                int totalPrice = 0;
-                for (OrderItem orderItem : orderItems){
-                    totalPrice += orderItem.getTotalPrice();
-                }
-                */
-                return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
-            }
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice(){
+        /*
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        */
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+    }
 
 
 }
