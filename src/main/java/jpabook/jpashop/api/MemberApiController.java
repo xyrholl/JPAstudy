93c9dfaf2015290afcb6c30f373badecce880681
LeeSangYoeb <xyrho.v@gmail.com>
2022-04-12 23:01:09 +0900

    Feat 회원수정 API 개발
    - 수정 후 영속성 컨택스트를 그대로 반환하지 않고 조회쿼리를 날리는 방식
    유지보수에 조금 더 유리함

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,12 +3,15 @@ package jpabook.jpashop.api;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -30,6 +33,27 @@ public class MemberApiController {
         member.setName(requset.getName());
         Long id = memberService.join(member);
         return new CreatememberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+        @PathVariable("id") Long id, 
+        @RequestBody @Valid UpdateMemberRequest requset){
+            memberService.update(id, requset.getName());
+            Member findMember = memberService.findOne(id);
+            return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
     }
 
     @Data
